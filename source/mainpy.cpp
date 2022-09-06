a594fd505fb8abf0b04818f64222ca71c749e37e
Chris Matichuk <matichuk@hotmail.com>
2022-09-06 16:13:18 -0700

    update grapapy

--- source/mainpy.cpp
+++ source/mainpy.cpp
@@ -67,7 +67,95 @@ int add(int i, int j) {
     return i + j;
 }
 
-PYBIND11_MODULE(grapapy, m) {
+template <typename... Args>
+using overload_cast_ = pybind11::detail::overload_cast_impl<Args...>;
+
+/* https://pybind11.readthedocs.io/en/stable/classes.html
+py::class_<Pet>(m, "Pet")
+    .def("set", overload_cast_<int>()(&Pet::set), "Set the pet's age")
+    .def("set", overload_cast_<const std::string &>()(&Pet::set), "Set the pet's name");
+*/
+
+class  GrapaStruct 
+{
+public:
+    GrapaScriptExec mScriptExec;
+    GrapaConsoleSend mConsoleSend;
+    GrapaMainResponse mConsoleResponse;
+    GrapaNames mRuleVariables;
+    GrapaStruct() 
+	{
+		mConsoleSend.mScriptState.vScriptExec = &mScriptExec;
+		mScriptExec.vScriptState = &mConsoleSend.mScriptState;
+		mConsoleSend.mScriptState.SetNameSpace(&mRuleVariables);
+		mRuleVariables.SetResponse(&mConsoleResponse);
+		mConsoleSend.Start();
+		GrapaCHAR grresult;
+		if (gSystem->mGrammar.mLength)
+			grresult = mConsoleSend.SendSync(gSystem->mGrammar);
+	}
+    ~GrapaStruct() 
+	{ 
+	}
+	
+    std::string eval(std::string cmdstr)
+	{
+		std::string r;
+		if (cmdstr.length())
+		{
+			GrapaCHAR runStr(cmdstr.c_str(), cmdstr.length());
+			GrapaCHAR grresult = mConsoleSend.SendSync(runStr);
+			r.assign((char*)grresult.mBytes, grresult.mLength);
+		}
+		return r; 
+	}
+	
+	py::bytes evalb(std::string cmdstr)
+	{
+		std::string r;
+		if (cmdstr.length())
+		{
+			GrapaCHAR runStr(cmdstr.c_str(), cmdstr.length());
+			GrapaCHAR grresult = mConsoleSend.SendSync(runStr);
+			r.assign((char*)grresult.mBytes, grresult.mLength);
+		}
+		return py::bytes(r);
+	}
+	
+	std::string exec(py::bytes cmdbytes)
+	{
+		std::string r;
+		std::string cmdstr = py::str(cmdbytes);
+		if (cmdstr.length())
+		{
+			GrapaCHAR runStr(cmdstr.c_str(), cmdstr.length());
+			GrapaCHAR grresult = mConsoleSend.SendSync(runStr);
+			r.assign((char*)grresult.mBytes, grresult.mLength);
+		}
+		return r; 
+	}
+
+	py::bytes execb(py::bytes cmdbytes)
+	{
+		std::string r;
+		std::string cmdstr = py::str(cmdbytes);
+		if (cmdstr.length())
+		{
+			GrapaCHAR runStr(cmdstr.c_str(), cmdstr.length());
+			GrapaCHAR grresult = mConsoleSend.SendSync(runStr);
+			r.assign((char*)grresult.mBytes, grresult.mLength);
+		}
+		return py::bytes(r);
+	}
+
+};
+
+PYBIND11_MODULE(grapapy, m)
+{
+	GrapaCHAR inStr, outStr, runStr;
+	bool needExit = false, showConsole = false, showWidget = false;
+	GrapaCHAR s = GrapaLink::Start(needExit, showConsole, showWidget, inStr, outStr, runStr);
+	
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
@@ -78,7 +166,6 @@ PYBIND11_MODULE(grapapy, m) {
            :toctree: _generate
 
            add
-           subtract
            eval
     )pbdoc";
 
@@ -86,23 +173,25 @@ PYBIND11_MODULE(grapapy, m) {
         Add two numbers
 
         Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
+    )pbdoc",
+      py::arg("i"), py::arg("j"));
 
     m.def("eval", &Grapaeval, R"pbdoc(
         Evaluate a Grapa script
 
         Some other explanation about the Grapaeval function.
-    )pbdoc");
+    )pbdoc",
+		py::arg("s"));
+	
+	py::class_<GrapaStruct>(m, "new")
+		.def(py::init<>())
+		.def("eval", &GrapaStruct::eval,"",py::arg("s"))
+		.def("evalb", &GrapaStruct::evalb,"",py::arg("s"))
+		.def("exec", &GrapaStruct::exec, "", py::arg("b"))
+		.def("exec", &GrapaStruct::execb, "", py::arg("b"))
+		;
 
-#ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-    m.attr("__version__") = "dev";
-#endif
+    m.attr("__version__") = "0.0.1";
+	
+	// GrapaLink::Stop();
 }
