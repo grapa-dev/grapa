0a43b853a27cb18c67fd8c434c1505c388800f89
Chris Matichuk <matichuk@hotmail.com>
2022-09-08 22:09:09 -0700

    update grapapy - with ORIGIN

--- setup.py
+++ setup.py
@@ -1,157 +1,132 @@
 import os
-import re
-import subprocess
 import sys
-import shutil
 import platform
+
+from distutils.command.build import build
 
 from setuptools import Extension, setup, find_packages, Command
 from setuptools.command.build_ext import build_ext
 
-# Convert distutils Windows platform specifiers to CMake -A arguments
-PLAT_TO_CMAKE = {
-    "win32": "Win32",
-    "win-amd64": "x64",
-    "win-arm32": "ARM",
-    "win-arm64": "ARM64",
-}
+from pathlib import Path
+
+extra_link_args = []
+runtime_library_dirs = []
+
+if sys.platform.startswith('win32'):
+    so_ext = '.dll'
+    so_ext = '.lib'
+    lib_filename = 'grapa' + so_ext
+    lib_pathfile = 'grapa-lib/win/' + lib_filename
+if sys.platform.startswith('linux'):
+    so_ext = '.so'
+    lib_filename = 'libgrapa' + so_ext
+    lib_pathfile = 'grapa-lib/ubuntu64/' + lib_filename
+    runtime_library_dirs = ['$ORIGIN']
+elif sys.platform.startswith('darwin'):
+    so_ext = '.dylib'
+    lib_filename = 'libgrapa' + so_ext
+    lib_pathfile = 'grapa-lib/mac-intel/' + lib_filename
+    extra_link_args = ['-Wl,-rpath,@loader_path']
 
 
-# A CMakeExtension needs a sourcedir instead of a file list.
-# The name must be the _single_ output extension from the CMake build.
-# If you need multiple extensions, see scikit-build.
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=""):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
+class CopySharedLibrary(Command):
+    user_options = []
 
-# runtime_library_dirs=["$ORIGIN"]
+    def initialize_options(self):
+        self.build_lib = None
+        self.inplace = 0
+        self.build_dir = "source"
+        self.filename = lib_filename
+        self.lib_source_path = os.path.join(self.build_dir, lib_pathfile)
+        self.package_name = 'source'
 
-class CMakeBuild(build_ext):
-    def build_extension(self, ext):
-    
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+    def finalize_options(self):
+        self.set_undefined_options('build', ('build_lib', 'build_lib'), )
+        self.set_undefined_options('build_ext', ('inplace', 'inplace'), )
 
-        # required for auto-detection & inclusion of auxiliary "native" libs
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
-
-        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
-        cfg = "Debug" if debug else "Release"
-
-        # CMake lets you override the generator - we need to check this.
-        # Can be set with Conda-Build, for example.
-        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
-
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
-        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
-        # from Python.
-        cmake_args = [
-            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
-            f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
-        ]
-        build_args = []
-        # Adding CMake arguments set as environment variable
-        # (needed e.g. to build for ARM OSx on conda-forge)
-        if "CMAKE_ARGS" in os.environ:
-            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
-
-        # In this example, we pass in the version to C++. You might not need to.
-        # cmake_args += [f"-DEXAMPLE_VERSION_INFO={self.distribution.get_version()}"]
-
-        if self.compiler.compiler_type != "msvc":
-            # Using Ninja-build since it a) is available as a wheel and b)
-            # multithreads automatically. MSVC would require all variables be
-            # exported for Ninja to pick it up, which is a little tricky to do.
-            # Users can override the generator with CMAKE_GENERATOR in CMake
-            # 3.15+.
-            if not cmake_generator or cmake_generator == "Ninja":
-                try:
-                    import ninja  # noqa: F401
-
-                    ninja_executable_path = os.path.join(ninja.BIN_DIR, "ninja")
-                    cmake_args += [
-                        "-GNinja",
-                        f"-DCMAKE_MAKE_PROGRAM:FILEPATH={ninja_executable_path}",
-                    ]
-                except ImportError:
-                    pass
-
+    def run(self) -> None:
+        self.inplace = self.get_finalized_command('build_ext').inplace
+        if self.inplace:
+            lib_target_path = self.package_name
         else:
+            lib_target_path = os.path.join(self.build_lib, self.package_name)
+            self.mkpath(lib_target_path)
+        
+        self.copy_file(self.lib_source_path, os.path.join(lib_target_path, self.filename))
 
-            # Single config generators are handled "normally"
-            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+        #self.lib_source_path = os.path.join(self.build_dir, "fl-lib/win", "fltk.lib")
+        #self.copy_file(self.lib_source_path, os.path.join(lib_target_path, "fltk.lib"))
+        
+        #self.mkpath(os.path.join(lib_target_path, "fl-lib/win"))
 
-            # CMake allows an arch-in-generator style for backward compatibility
-            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+        #source_path = Path(__file__).resolve()
+        #source_dir = source_path.parent
+        #self.lib_source_path = os.path.join(source_dir, "source/fl-lib/win")
+        #for file_name in os.listdir(self.lib_source_path):
+        #    self.copy_file(os.path.join(self.lib_source_path,file_name), os.path.join(lib_target_path, "fl-lib/win", file_name))
+       
+        os.environ["ORIGIN"] = os.path.abspath(lib_target_path)
 
-            # Specify the arch if using MSVC generator, but only if it doesn't
-            # contain a backward-compatibility arch spec already in the
-            # generator name.
-            if not single_config and not contains_arch:
-                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
 
-            # Multi-config generators have a different way to specify configs
-            if not single_config:
-                cmake_args += [
-                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
-                ]
-                build_args += ["--config", cfg]
+class CustomBuild(build):
+    sub_commands = [
+        ('build_clib', build.has_c_libraries),
+        ('build_ext', build.has_ext_modules),
+        ('build_py', build.has_pure_modules),
+        ('build_scripts', build.has_scripts),
+    ]
 
-        if sys.platform.startswith("darwin"):
-            # Cross-compile support for macOS - respect ARCHFLAGS if set
-            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
-            if archs:
-                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
 
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call, not supported by pip or PyPA-build.
-            if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
-                build_args += [f"-j{self.parallel}"]
+class CustomBuildExt(build_ext):
 
-        build_temp = os.path.join(self.build_temp, ext.name)
-        if not os.path.exists(build_temp):
-            os.makedirs(build_temp)
+    def run(self):
+        self.run_command('copy_grapalib')
+        super().run()
 
-        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=build_temp)
-        subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=build_temp)
-
-        my_system = platform.system()
-
-        if sys.platform.startswith('linux'):
-            shutil.copy(os.path.join(ext.sourcedir,"source/grapa-lib/ubuntu64/libgrapa.so"), os.path.join(build_temp,"libgrapa.so"))
-            for file_name in os.listdir("source/grapa-lib/aws"):
-                source = os.path.join(ext.sourcedir,"source/grapa-lib/aws/" + file_name)
-                destination = os.path.join(build_temp,file_name)
-                shutil.copy(source, destination)
-
-def pick_data_files():
+def pick_library_dirs():
     my_system = platform.system()
     if my_system == 'Linux':
-        return [('', ['source/grapa-lib/ubuntu64/libgrapa.so'])]
+        return ["source", "source/grapa-lib/ubuntu64"]
     if my_system == 'Darwin':
-        return [('', ['source/grapa-lib/mac-intel/libgrapa.so'])]
+        return ["source", "source/grapa-lib/mac-intel"]
     if my_system == 'Windows':
-        return []
+        return ["source", "source/grapa-lib/win"]
     raise ValueError("Unknown platform: " + my_system)
-    
-# The information here can also be placed in setup.cfg - better separation of
-# logic and declaration, and simpler if you include description/version in a file.
+
+def pick_libraries():
+    my_system = platform.system()
+    if my_system == 'Linux':
+        return ["grapa"]
+    if my_system == 'Darwin':
+        return ["grapa"]
+    if my_system == 'Windows':
+        return ["grapa","Gdi32","Advapi32","User32","Ole32","Shell32","Comdlg32"]
+    raise ValueError("Unknown platform: " + my_system)
+
+lib_grapa = Extension(
+    'grapapy', 
+    sources = ['source/mainpy.cpp'],
+    include_dirs=["source"],
+    library_dirs=pick_library_dirs(),
+    libraries=pick_libraries(),
+    runtime_library_dirs=runtime_library_dirs,
+    extra_link_args=extra_link_args
+)
+
 setup(
     name="grapapy",
-    version="0.0.2",
+    version="0.0.3",
     author="Chris Matichuk",
     author_email="matichuk@hotmail.com",
     description="grammar parser language",
     long_description="",
-    ext_modules=[CMakeExtension("grapapy")],
-    cmdclass={"build_ext": CMakeBuild},
+    ext_modules=[lib_grapa],
+    cmdclass={
+        'copy_grapalib': CopySharedLibrary,
+        'build_ext': CustomBuildExt,
+        'build': CustomBuild,
+    },
     zip_safe=False,
     python_requires=">=3.6",
-    data_files=pick_data_files(),
+    packages=find_packages(),
 )
