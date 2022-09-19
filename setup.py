bb7f6506e1df3817d702b6e15e51c7081dcf3521
Chris Matichuk <matichuk@hotmail.com>
2022-09-18 18:15:22 -0700

    update version to 0.0.12

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from pathlib import Path
 extra_link_args = []
 extra_compile_args = []
 runtime_library_dirs = []
-grapapy_version = "0.0.11"
+grapapy_version = "0.0.12"
 is_aws = False
 is_apple = False
 from_os = ''
--- source/mainpy.cpp
+++ source/mainpy.cpp
@@ -348,7 +348,7 @@ PYBIND11_MODULE(grapapy, m)
 		py::arg("b"));
 	*/
 	
-    m.attr("__version__") = "0.0.11";
+    m.attr("__version__") = "0.0.12";
 	
 	// GrapaLink::Stop();
 }
