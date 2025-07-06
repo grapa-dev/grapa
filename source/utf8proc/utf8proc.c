bb1fba676b4edaa27c75c0b5946ac9988f89a1ef
Chris Matichuk <matichuk@hotmail.com>
2025-07-06 09:58:13 -0600

    use earlier version of utf8proc

--- source/utf8proc/utf8proc.c
+++ source/utf8proc/utf8proc.c
@@ -101,7 +101,7 @@ UTF8PROC_DLLEXPORT const char *utf8proc_version(void) {
 }
 
 UTF8PROC_DLLEXPORT const char *utf8proc_unicode_version(void) {
-  return "17.0.0";
+  return "15.0.0";
 }
 
 UTF8PROC_DLLEXPORT const char *utf8proc_errmsg(utf8proc_ssize_t errcode) {
@@ -288,54 +288,35 @@ static utf8proc_bool grapheme_break_simple(int lbc, int tbc) {
     true; // GB999
 }
 
-static utf8proc_bool grapheme_break_extended(int lbc, int tbc, int licb, int ticb, utf8proc_int32_t *state)
+static utf8proc_bool grapheme_break_extended(int lbc, int tbc, utf8proc_int32_t *state)
 {
   if (state) {
-    int state_bc, state_icb; /* boundclass and indic_conjunct_break state */
-    if (*state == 0) { /* state initialization */
-      state_bc = lbc;
-      state_icb = licb == UTF8PROC_INDIC_CONJUNCT_BREAK_CONSONANT ? licb : UTF8PROC_INDIC_CONJUNCT_BREAK_NONE;
-    }
-    else { /* lbc and licb are already encoded in *state */
-      state_bc = *state & 0xff;  // 1st byte of state is bound class
-      state_icb = *state >> 8;   // 2nd byte of state is indic conjunct break
-    }
-
-    utf8proc_bool break_permitted = grapheme_break_simple(state_bc, tbc) &&
-       !(state_icb == UTF8PROC_INDIC_CONJUNCT_BREAK_LINKER
-        && ticb == UTF8PROC_INDIC_CONJUNCT_BREAK_CONSONANT); // GB9c
-
-    // Special support for GB9c.  Don't break between two consonants
-    // separated 1+ linker characters and 0+ extend characters in any order.
-    // After a consonant, we enter LINKER state after at least one linker.
-    if (ticb == UTF8PROC_INDIC_CONJUNCT_BREAK_CONSONANT
-        || state_icb == UTF8PROC_INDIC_CONJUNCT_BREAK_CONSONANT
-        || state_icb == UTF8PROC_INDIC_CONJUNCT_BREAK_EXTEND)
-      state_icb = ticb;
-    else if (state_icb == UTF8PROC_INDIC_CONJUNCT_BREAK_LINKER)
-      state_icb = ticb == UTF8PROC_INDIC_CONJUNCT_BREAK_EXTEND ?
-                  UTF8PROC_INDIC_CONJUNCT_BREAK_LINKER : ticb;
+    int lbc_override;
+    if (*state == UTF8PROC_BOUNDCLASS_START)
+      *state = lbc_override = lbc;
+    else
+      lbc_override = *state;
+    utf8proc_bool break_permitted = grapheme_break_simple(lbc_override, tbc);
 
     // Special support for GB 12/13 made possible by GB999. After two RI
     // class codepoints we want to force a break. Do this by resetting the
     // second RI's bound class to UTF8PROC_BOUNDCLASS_OTHER, to force a break
     // after that character according to GB999 (unless of course such a break is
     // forbidden by a different rule such as GB9).
-    if (state_bc == tbc && tbc == UTF8PROC_BOUNDCLASS_REGIONAL_INDICATOR)
-      state_bc = UTF8PROC_BOUNDCLASS_OTHER;
+    if (*state == tbc && tbc == UTF8PROC_BOUNDCLASS_REGIONAL_INDICATOR)
+      *state = UTF8PROC_BOUNDCLASS_OTHER;
     // Special support for GB11 (emoji extend* zwj / emoji)
-    else if (state_bc == UTF8PROC_BOUNDCLASS_EXTENDED_PICTOGRAPHIC) {
+    else if (*state == UTF8PROC_BOUNDCLASS_EXTENDED_PICTOGRAPHIC) {
       if (tbc == UTF8PROC_BOUNDCLASS_EXTEND) // fold EXTEND codepoints into emoji
-        state_bc = UTF8PROC_BOUNDCLASS_EXTENDED_PICTOGRAPHIC;
+        *state = UTF8PROC_BOUNDCLASS_EXTENDED_PICTOGRAPHIC;
       else if (tbc == UTF8PROC_BOUNDCLASS_ZWJ)
-        state_bc = UTF8PROC_BOUNDCLASS_E_ZWG; // state to record emoji+zwg combo
+        *state = UTF8PROC_BOUNDCLASS_E_ZWG; // state to record emoji+zwg combo
       else
-        state_bc = tbc;
+        *state = tbc;
     }
     else
-      state_bc = tbc;
+      *state = tbc;
 
-    *state = state_bc + (state_icb << 8);
     return break_permitted;
   }
   else
@@ -345,12 +326,8 @@ static utf8proc_bool grapheme_break_extended(int lbc, int tbc, int licb, int tic
 UTF8PROC_DLLEXPORT utf8proc_bool utf8proc_grapheme_break_stateful(
     utf8proc_int32_t c1, utf8proc_int32_t c2, utf8proc_int32_t *state) {
 
-  const utf8proc_property_t *p1 = utf8proc_get_property(c1);
-  const utf8proc_property_t *p2 = utf8proc_get_property(c2);
-  return grapheme_break_extended(p1->boundclass,
-                                 p2->boundclass,
-                                 p1->indic_conjunct_break,
-                                 p2->indic_conjunct_break,
+  return grapheme_break_extended(utf8proc_get_property(c1)->boundclass,
+                                 utf8proc_get_property(c2)->boundclass,
                                  state);
 }
 
@@ -430,10 +407,6 @@ UTF8PROC_DLLEXPORT int utf8proc_isupper(utf8proc_int32_t c)
    hopefully less buggy than most system wcwidth functions). */
 UTF8PROC_DLLEXPORT int utf8proc_charwidth(utf8proc_int32_t c) {
   return utf8proc_get_property(c)->charwidth;
-}
-
-UTF8PROC_DLLEXPORT utf8proc_bool utf8proc_charwidth_ambiguous(utf8proc_int32_t c) {
-  return utf8proc_get_property(c)->ambiguous_width;
 }
 
 UTF8PROC_DLLEXPORT utf8proc_category_t utf8proc_category(utf8proc_int32_t c) {
@@ -525,8 +498,8 @@ UTF8PROC_DLLEXPORT utf8proc_ssize_t utf8proc_decompose_char(utf8proc_int32_t uc,
   }
   if (options & UTF8PROC_CHARBOUND) {
     utf8proc_bool boundary;
-    boundary = grapheme_break_extended(0, property->boundclass, 0, property->indic_conjunct_break,
-                                       last_boundclass);
+    int tbc = property->boundclass;
+    boundary = grapheme_break_extended(*last_boundclass, tbc, last_boundclass);
     if (boundary) {
       if (bufsize >= 1) dst[0] = -1; /* sentinel value for grapheme break */
       if (bufsize >= 2) dst[1] = uc;
@@ -646,13 +619,15 @@ UTF8PROC_DLLEXPORT utf8proc_ssize_t utf8proc_normalize_utf32(utf8proc_int32_t *b
   }
   if (options & UTF8PROC_COMPOSE) {
     utf8proc_int32_t *starter = NULL;
-    const utf8proc_property_t *starter_property = NULL;
+    utf8proc_int32_t current_char;
+    const utf8proc_property_t *starter_property = NULL, *current_property;
     utf8proc_propval_t max_combining_class = -1;
     utf8proc_ssize_t rpos;
     utf8proc_ssize_t wpos = 0;
+    utf8proc_int32_t composition;
     for (rpos = 0; rpos < length; rpos++) {
-      utf8proc_int32_t current_char = buffer[rpos];
-      const utf8proc_property_t *current_property = unsafe_get_property(current_char);
+      current_char = buffer[rpos];
+      current_property = unsafe_get_property(current_char);
       if (starter && current_property->combining_class > max_combining_class) {
         /* combination perhaps possible */
         utf8proc_int32_t hangul_lindex;
@@ -683,29 +658,22 @@ UTF8PROC_DLLEXPORT utf8proc_ssize_t utf8proc_normalize_utf32(utf8proc_int32_t *b
         if (!starter_property) {
           starter_property = unsafe_get_property(*starter);
         }
-        int idx = starter_property->comb_index;
-        if (idx < 0x3FF && current_property->comb_issecond) {
-          int len = starter_property->comb_length;
-          utf8proc_int32_t max_second = utf8proc_combinations_second[idx + len - 1];
-          if (current_char <= max_second) {
-            int off;
-            // TODO: binary search? arithmetic search?
-            for (off = 0; off < len; ++off) {
-              utf8proc_int32_t second = utf8proc_combinations_second[idx + off];
-              if (current_char < second) {
-                /* not found */
-                break;
-              }
-              if (current_char == second) {
-                /* found */
-                utf8proc_int32_t composition = utf8proc_combinations_combined[idx + off];
-                *starter = composition;
-                starter_property = NULL;
-                break;
-              }
-            }
-            if (starter_property == NULL) {
-              /* found */
+        if (starter_property->comb_index < 0x8000 &&
+            current_property->comb_index != UINT16_MAX &&
+            current_property->comb_index >= 0x8000) {
+          int sidx = starter_property->comb_index;
+          int idx = current_property->comb_index & 0x3FFF;
+          if (idx >= utf8proc_combinations[sidx] && idx <= utf8proc_combinations[sidx + 1] ) {
+            idx += sidx + 2 - utf8proc_combinations[sidx];
+            if (current_property->comb_index & 0x4000) {
+              composition = (utf8proc_combinations[idx] << 16) | utf8proc_combinations[idx+1];
+            } else
+              composition = utf8proc_combinations[idx];
+
+            if (composition > 0 && (!(options & UTF8PROC_STABLE) ||
+                !(unsafe_get_property(composition)->comp_exclusion))) {
+              *starter = composition;
+              starter_property = NULL;
               continue;
             }
           }
