e9c0b8a952a7139c2c85d4f1e65a78d6d6bdd33d
Chris Matichuk <matichuk@hotmail.com>
2025-07-19 12:22:07 -0500

    Update build.py

--- build.py
+++ build.py
@@ -345,15 +345,17 @@ class GrapaBuilder:
             else:
                 # Build shared library
                 cpp_files = glob.glob("source/grapa/*.cpp")
-                lib_files = []
-                for lib_path in config.libs:
-                    lib_files.extend(glob.glob(lib_path))
+                openssl_libs = glob.glob(f"source/openssl-lib/{config.target}/*.a")
+                fl_libs = glob.glob(f"source/fl-lib/{config.target}/*.a")
+                blst_libs = glob.glob(f"source/blst-lib/{config.target}/*.a")
+                pcre2_lib = glob.glob(f"source/pcre2-lib/{config.target}/libpcre2-8.a")
                 
-                cmd = ["g++", "-shared", "-Isource", "-DUTF8PROC_STATIC"] + cpp_files + ["utf8proc.o"]
-                cmd.extend(lib_files)
-                cmd.extend([f"-L{lib_path}", "-std=c++17", "-lX11", "-lXfixes", "-lXft", "-lXext",
-                    "-lXrender", "-lXinerama", "-lfontconfig", "-lXcursor", "-ldl",
-                    "-lm", "-static-libgcc", "-O3", "-pthread", "-fPIC", "-o", "libgrapa.so"])
+                cmd = ["g++", "-shared", "-Isource", "-DUTF8PROC_STATIC"] + cpp_files + ["source/utf8proc/utf8proc.c"] + openssl_libs + fl_libs + blst_libs + pcre2_lib + [
+                    f"-Lsource/openssl-lib/{config.target}", "-std=c++17", "-lcrypto",
+                    "-lX11", "-lXfixes", "-lXft", "-lXext", "-lXrender", "-lXinerama",
+                    "-lfontconfig", "-lXcursor", "-ldl", "-lm", "-static-libgcc",
+                    "-O3", "-pthread", "-fPIC", "-o", "libgrapa.so"
+                ]
                 
                 subprocess.run(cmd, check=True)
                 shutil.copy("libgrapa.so", f"source/grapa-lib/{config.target}/libgrapa.so")
@@ -368,8 +370,8 @@ class GrapaBuilder:
             
             cmd = [
                 "g++", "-Isource", "-DUTF8PROC_STATIC", "source/main.cpp"
-            ] + cpp_files + ["utf8proc.o"] + openssl_libs + fl_libs + blst_libs + [
-                f"source/pcre2-lib/{config.target}/libpcre2-8.a", f"-Lsource/openssl-lib/{config.target}", "-std=c++17", 
+            ] + cpp_files + ["source/utf8proc/utf8proc.c"] + openssl_libs + fl_libs + blst_libs + [
+                f"source/pcre2-lib/{config.target}/libpcre2-8.a", f"-Lsource/openssl-lib/{config.target}", "-std=c++17", "-lcrypto", 
                 "-lX11", "-lXfixes", "-lXft", "-lXext", "-lXrender", "-lXinerama", 
                 "-lfontconfig", "-lXcursor", "-ldl", "-lm", "-static-libgcc", 
                 "-O3", "-pthread", "-o", config.output_name
@@ -446,7 +448,7 @@ class GrapaBuilder:
                 pcre2_lib = glob.glob(f"source/pcre2-lib/{config.target}/libpcre2-8.a")
                 
                 cmd = ["g++", "-shared", "-Isource", "-DUTF8PROC_STATIC"] + cpp_files + ["source/utf8proc/utf8proc.c"] + openssl_libs + fl_libs + blst_libs + pcre2_lib + [
-                    f"-Lsource/openssl-lib/{config.target}", "-std=c++17",
+                    f"-Lsource/openssl-lib/{config.target}", "-std=c++17", "-lcrypto",
                     "-lX11", "-lXfixes", "-lXft", "-lXext", "-lXrender", "-lXinerama",
                     "-lfontconfig", "-lXcursor", "-ldl", "-lm", "-static-libgcc",
                     "-O3", "-pthread", "-fPIC", "-o", "libgrapa.so"
@@ -466,8 +468,8 @@ class GrapaBuilder:
             
             cmd = [
                 "g++", "-Isource", "-DUTF8PROC_STATIC", "source/main.cpp"
-            ] + cpp_files + ["utf8proc.o"] + openssl_libs + fl_libs + blst_libs + [
-                f"source/pcre2-lib/{config.target}/libpcre2-8.a", f"-Lsource/openssl-lib/{config.target}", "-std=c++17", 
+            ] + cpp_files + ["source/utf8proc/utf8proc.c"] + openssl_libs + fl_libs + blst_libs + [
+                f"source/pcre2-lib/{config.target}/libpcre2-8.a", f"-Lsource/openssl-lib/{config.target}", "-std=c++17", "-lcrypto", 
                 "-lX11", "-lXfixes", "-lXft", "-lXext", "-lXrender", "-lXinerama", 
                 "-lfontconfig", "-lXcursor", "-ldl", "-lm", "-static-libgcc", 
                 "-O3", "-pthread", "-o", config.output_name
