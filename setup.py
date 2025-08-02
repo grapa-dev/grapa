aae54d33eb39db939f45d8fefc87528ff62c0179
Grapa Development Team <grapa-dev@example.com>
2025-08-02 15:42:32 -0500

    Update version to 0.0.51 for multi-platform testing

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_pybind11_include():
 extra_link_args = []
 extra_compile_args = []
 runtime_library_dirs = []
-grapapy_version = "0.0.50"
+grapapy_version = "0.0.51"
 is_aws = False
 is_apple = False
 from_os = ''
