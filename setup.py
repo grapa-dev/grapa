8837ce1d11b40af5128ad32084d49a8733f8da10
Chris Matichuk <matichuk@hotmail.com>
2025-08-02 11:28:53 -0500

    Bump version to 0.0.49 for new wheel build

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_pybind11_include():
 extra_link_args = []
 extra_compile_args = []
 runtime_library_dirs = []
-grapapy_version = "0.0.43"
+grapapy_version = "0.0.49"
 is_aws = False
 is_apple = False
 from_os = ''
