60ced8278874d2fd1d8cfa10489c175d92450646
Chris Matichuk <matichuk@hotmail.com>
2025-08-03 16:57:40 -0500

    Bump version to 0.0.132

--- source/grapa/GrapaLink.h
+++ source/grapa/GrapaLink.h
@@ -3,7 +3,7 @@
 
 #include "GrapaValue.h"
 
-#define grapa_version "0.0.131"
+#define grapa_version "0.0.132"
 
 typedef void (*GrapaLinkCallback)(const char* pStr, bool pFlush);
 class GrapaSystem;
