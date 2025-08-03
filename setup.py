a1bd0f8afd7f544cedf8511e01479a7e8e231496
Chris Matichuk <matichuk@hotmail.com>
2025-08-03 15:37:00 -0500

    Bump version to 0.0.121

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_pybind11_include():
 extra_link_args = []
 extra_compile_args = []
 runtime_library_dirs = []
-grapapy_version = "0.0.120"
+grapapy_version = "0.0.121"
 is_aws = False
 is_apple = False
 from_os = ''
