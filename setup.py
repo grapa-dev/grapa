35ebebf0244fe13ac234073bf9b710265ef7f6da
Grapa Development Team <grapa-dev@example.com>
2025-08-04 11:47:54 -0500

    Bump version to 0.0.182

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_pybind11_include():
 extra_link_args = []
 extra_compile_args = []
 runtime_library_dirs = []
-grapapy_version = "0.0.181"
+grapapy_version = "0.0.182"
 is_aws = False
 is_apple = False
 from_os = ''
--- source/grapa/GrapaLink.h
+++ source/grapa/GrapaLink.h
@@ -3,7 +3,7 @@
 
 #include "GrapaValue.h"
 
-#define grapa_version "0.0.181"
+#define grapa_version "0.0.182"
 
 typedef void (*GrapaLinkCallback)(const char* pStr, bool pFlush);
 class GrapaSystem;
