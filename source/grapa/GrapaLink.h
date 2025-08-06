e017a225246e209defdff2440ce656c9ee1033a4
Grapa Development Team <grapa-dev@example.com>
2025-08-06 14:20:27 -0500

    Bump version to 0.0.245

--- source/grapa/GrapaLink.h
+++ source/grapa/GrapaLink.h
@@ -3,7 +3,7 @@
 
 #include "GrapaValue.h"
 
-#define grapa_version "0.0.244"
+#define grapa_version "0.0.245"
 
 typedef void (*GrapaLinkCallback)(const char* pStr, bool pFlush);
 class GrapaSystem;
