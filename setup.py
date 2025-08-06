d220952eb70f3dd275f57dfaf82c4441bd0cdcef
Grapa Development Team <grapa-dev@example.com>
2025-08-06 15:41:35 -0500

    Bump version to 0.0.249

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_pybind11_include():
 extra_link_args = []
 extra_compile_args = []
 runtime_library_dirs = []
-grapapy_version = "0.0.248"
+grapapy_version = "0.0.249"
 
 is_apple = False
 from_os = ''
