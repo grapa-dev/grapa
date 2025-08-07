a5427929171924d49128d6b3edc0aedad83a1e6e
Grapa Development Team <grapa-dev@example.com>
2025-08-07 00:04:19 -0500

    version checkin

--- source/grapa/GrapaLink.h
+++ source/grapa/GrapaLink.h
@@ -3,7 +3,7 @@
 
 #include "GrapaValue.h"
 
-#define grapa_version "0.0.261"
+#define grapa_version "0.0.264"
 
 typedef void (*GrapaLinkCallback)(const char* pStr, bool pFlush);
 class GrapaSystem;
