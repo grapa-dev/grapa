da248c04b5dca203bc3252a4c0dd75e8a22992c9
Chris Matichuk <matichuk@hotmail.com>
2025-08-22 22:49:36 -0500

    Update GrapaState.cpp

--- source/grapa/GrapaState.cpp
+++ source/grapa/GrapaState.cpp
@@ -4666,11 +4666,7 @@ GrapaRuleEvent* GrapaScriptExec::ProcessPlan(GrapaNames* pNameSpace, GrapaRuleEv
 					}
 					
 					result = libName->vLibraryEvent->Run(this, pNameSpace, libParam, input);
-					if (result)
-					{
-						if (result->mControlFlow == GrapaControlFlowType::RETURN)
-							result->mControlFlow = 0;
-					}
+
 					// ADD DEBUG: Function result (Level 4)
 					if (vScriptState->mDebug.ShouldDebug("executor", 4)) {
 						char debugMsg[256];
