925c9575a6b40769b4b609983297f45e588be4c5
Grapa Development Team <grapa-dev@example.com>
2025-09-01 15:50:20 -0500

    fix sleep on non-windows

--- source/grapa/GrapaType.h
+++ source/grapa/GrapaType.h
@@ -24,6 +24,11 @@ typedef s32                 GrapaError;
 #define NULL 0LL
 #endif
 
+// Enable threading support for all platforms
+#if defined(__MINGW32__) || defined(__GNUC__) || defined(_WIN32)
+#define _THREAD_
+#endif
+
 #endif //_GrapaType_
 
 ////////////////////////////////////////////////////////////////////////////////
